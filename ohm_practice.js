5591fa0258500a128644c0c9ca6819dd0336a2c0
meganmrichardson <megan.marie.richardson@gmail.com>
2021-04-21 14:34:50 -0700

    Final commit - 100% on tests

--- ohm_practice.js
+++ ohm_practice.js
@@ -12,7 +12,12 @@ const grammars = {
   c = "2" (("3".."6" d d) | ("2" "2" "1".."9") | ("2" "3".."9" d) | ("7" "0".."1" d) | ("7" "2" "0")) x
   x = d d d d d d d d d d d d
   d = "0".."9"`,
-  adaFloat: `\d+[#|e|E|.|_|\-|fD|\+|\d]*`,
+  adaFloat: `a = n* (b | d)
+  d = ("." n)? e?
+  b = "#" c ("." c)? "#" e?
+  c = ("0".."9" | "A".."F" | "a".."f") ("_"? ("0".."9" | "A".."F" | "a".."f"))*
+  e = ("e" | "E") ("+"|"-")? n
+  n = "0".."9" ("_"? "0".."9")*`,
   notThreeEndingInOO: `a = ( d (b | c | e | f | g | h)? )?
   b = d ("A".."N" | "P".."Z")
   c = d ("a".."n" | "p".."z") 
@@ -26,13 +31,14 @@ const grammars = {
   eightThroughTwentyNine: `a = s | d
     s = "8".."9"
     d = "1".."2" "0".."9"`,
-  mLComment: `a = "(*" " "? ((~("*)" end) alnum | ~("*)" end) "*" | ~("*)" end) "(*")+) " "? "*)"`,
-  notDogDoorDenNoLookAround: `n = ("dog" | "door" | "den") ("A".."Z" | "a".."z")+ 
-  | "doo" ("A".."Z" | "a".."q" | "t".."z" )*
-  | "do" ("A".."Z" | "a".."f" | "h".."n" | "q".."z")*
-  | "de" ("A".."Z" | "a".."m" | "o".."z" )*
-  | "d" ("A".."Z" | "a".."d" | "f".."n" | "p".."z" )*
-  | ("A".."Z" | "a".."c" | "e".."z" )(("A".."Z" | "a".."z" )*)?`,
+  mLComment: `a = "(*" " "? (~("*)" end) (alnum | "*" | "(*"))* " "? "*)"`,
+  notDogDoorDenNoLookAround: `n = (a | b | c)?
+  a = ("door" | "den" | "dog") ("A".."Z" | "a".."z")+  
+  b = "doo" ("A".."Z" | "a".."q" | "s".."z") ("A".."Z" | "a".."z" )*     
+| "do" ("A".."Z" | "a".."f" | "h".."n" | "p".."z") ("A".."Z" | "a".."z" )*  
+| "de" ("A".."Z" | "a".."m" | "o".."z") ("A".."Z" | "a".."z" )*  
+| "d" ("A".."Z" | "a".."d" | "f".."n" | "p".."z" ) ("A".."Z" | "a".."z" )*      
+  c = ("A".."Z" | "a".."c" | "e".."z" )("A".."Z" | "a".."z" )*`,
   notDogDoorDenWithLookAround: `nond = ~(("dog" | "door" | "den") end) ("A".."Z" | "a".."z")*`
 };
 
--- regex_practice.js
+++ regex_practice.js
@@ -2,12 +2,12 @@ const regexes = {
   canadianPostalCode: /^([^DFIOQUa-z]|\d){3}[ ]([^DFIOQUa-z]|\d){3}$/,
   visa: /^4((\d{15})|(\d{12}))$/,
   masterCard: /^((5[1-5])|(2[221-720]))\d{14}$/,
-  adaFloat: /^\d+([#|e|E|.|_|\-|fD|\+|\d]+\d)*([#|\d]?)$/,
+  adaFloat: /^\d(_?\d)*(((\.\d(_?\d)*)?((e|E)(\+|-)?\d(_?\d)*)?)|(#(\d|[A-F]|[a-f])(_?(\d|[A-F]|[a-f]))*(\.(\d|[A-F]|[a-f])(_?(\d|[A-F]|[a-f]))*)?#((e|E)(\+|-)?\d(_?\d)*)?))$/,
   notThreeEndingInOO: /^($^)|((?!([a-zA-Z][oO]{2})\b)\b[a-zA-Z]+)$/,
-  divisibleBy64: /^([0|1]*000000)|(0+)$/,
-  eightThroughTwentyNine: /^[8-9]|[1-2][0-9]\b$/,
+  divisibleBy64: /^((((?!(000000)\b)(0|1))+000000)|0+)$/,
+  eightThroughTwentyNine: /^([8-9]|[1-2][0-9]\b)$/,
   mLComment: /^\(\*[\d\*\( ]*\*\)$/,
-  notDogDoorDenNoLookAround: /^((dog[a-z]+)|(den[a-z]+)|(door[a-z]+)|[a-ce-zA-Z]\w*)?$/,
+  notDogDoorDenNoLookAround: /^(($^)|(dog[a-z]+)|(den[a-z]+)|(door[a-z]+)|(doo[A-Za-qs-z][A-Za-z]*)|(do[A-Za-fh-np-z][A-Za-z]*)|(de[A-Za-mo-z][A-Za-z]*)|(d[A-Za-df-np-z][A-Za-z]*)|([a-ce-zA-Z]\w*))$/,
   notDogDoorDenWithLookAround: /^((?!(dog|door|den)\b)\b\w+)?$/
 };
 
